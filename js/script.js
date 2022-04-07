b28aa31e4720d07d2762dd7d0af7e48d8781d6cb
Santiago Mercado <102555836+SantyMerkdo@users.noreply.github.com>
2022-04-07 18:59:50 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("Página Hecha por Santiago Mercado");
+console.log("Plantilla de Página Web Oficial de Bootstrap");
+
+var fecha = "7/04/2022";
+console.log("Página Creada el " + fecha);
